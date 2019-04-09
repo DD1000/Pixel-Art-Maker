a496f7250cd536ff171ce71a16ee3bc6f0781c7d
Alexander Sanchez <sanchezalexander1000@gmail.com>
2019-04-09 17:27:03 -0400

    Complete Project

--- designs.js
+++ designs.js
@@ -1,10 +1,43 @@
-// Select color input
-// Select size input
+colorPicker = document.getElementById('colorPicker');
+submit = document.getElementById('sizePicker').lastElementChild;
+heightForm = document.getElementById('inputHeight');
+widthForm = document.getElementById('inputWidth');
 
-// When size is submitted by the user, call makeGrid()
+submit.addEventListener('click', function(event){
+  event.preventDefault();
+  makeGrid();
+});
 
-function makeGrid() {
+function makeGrid(event) {
 
-// Your code goes here!
+  canvas = document.querySelector('#pixelCanvas');
+  if(canvas.hasChildNodes())
+  {
+    grid = canvas.querySelector('div');
+    grid.parentNode.removeChild(grid);
+  }
 
+  var flexBox = document.createElement('div');
+  flexBox.addEventListener('click', function(event){
+  var clickedElem = event.target;
+  var color = colorPicker.value;
+  clickedElem.style.backgroundColor = color;
+})
+  document.querySelector('#pixelCanvas').appendChild(flexBox)
+  flexBox.setAttribute('style', 'width: 300px; height: 300px; border-style: solid; border: 10px; display: flex; flex-wrap: wrap; padding: 0px; margin: 0px;');
+  height = heightForm.value;
+  width = widthForm.value;
+  var gridBoxWidth = (300/width)-6;
+  var gridBoxHeight = (300/height)-6;
+  var integer = 9;
+  for (var i = 0; i < width; i++)
+  {
+    for(var j = 0; j < height; j++)
+    {
+      var gridBox = document.createElement('div');
+      document.querySelector('#pixelCanvas').firstElementChild.appendChild(gridBox)
+      gridBox.setAttribute('style', 'width: ' + gridBoxWidth + 'px; height: ' + gridBoxHeight + 'px; border-style: solid; padding: 0px; margin: 0px;');
+      gridBox.id = 'gridBox';
+    }
+  }
 }
